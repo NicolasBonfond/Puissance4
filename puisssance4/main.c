bd119184712251084037c3fca4404356e278f511
Nicolas Bonfond <43850060+NicolasBonfond@users.noreply.github.com>
2022-03-05 17:41:40 +0100

    Initial Commit

---
+++ puisssance4/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  puisssance4
+//
+//  Created by Nicolas Bonfond on 05/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
